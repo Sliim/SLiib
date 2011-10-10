cf1ea1a169a6a7797dced9403841d234fd5818a0
Sliim <sliim@mailoo.org>
2011-10-10 21:01:16 +0200

    #26 - Découpage de Confini.php en fonction de la nouvelle arborescence.

---
+++ php/library/Config.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * This source file is part of SLiib.
+ *
+ * SLiib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SLiib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with SLiib. If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ *
+ * PHP version 5
+ *  
+ * @category SLiib
+ * @package  Library
+ * @author   Sliim <sliim@mailoo.org>
+ * @license  GNU/GPL http://www.gnu.org/licenses/gpl-3.0.html
+ * @version  Release: 0.1
+ * @link     http://www.sliim-projects.eu
+ */
+
+/**
+ * SLiib_Config
+ * 
+ * @package Config
+ */
+abstract class SLiib_Config
+{
+
+  /**
+   * Tableau des différentes directives que contient le fichier
+   * 
+   * @var array
+   */
+  protected $_directives = array();
+
+  /**
+   * Fichier de configuration à manipuler
+   * 
+   * @var string
+   */
+  protected $_configFile;
+
+
+  /**
+   * Constructeur. Charge le fichier de configuration passé en paramètre
+   * 
+   * @param string $file Fichier à charger
+   * 
+   * @throws SLiib_Config_Exception
+   * 
+   * @return void
+   */
+  public function __construct($file)
+  {
+    $this->_configFile = $file;
+
+    if (!file_exists($file)) {
+      throw new SLiib_Config_Exception('File ' . $file . ' not found');
+    }
+
+  }
+
+
+  /**
+   * Récupère la valeur d'une directive.
+   * 
+   * @param string           $dirName Nom de la directive à récupérer
+   * @param string[optional] $block   Block dans lequel se trouve la directive.
+   * 
+   * @throws SLiib_Config_Exception
+   * 
+   * @return string Valeur de la directive.
+   */
+  public function getValue($dirName, $block=null)
+  {
+    if (is_null($block)) {
+      if (key_exists($dirName, $this->_directives))
+        return $this->_directives[$dirName];
+      else
+        throw new SLiib_Config_Exception(
+            'Directive {' . $dirName . '} not found'
+        );
+    } else {
+      if (key_exists($block, $this->_directives)
+        && is_array($this->_directives[$block])) {
+        if (key_exists($dirName, $this->_directives[$block]))
+          return $this->_directives[$block][$dirName];
+        else
+          throw new SLiib_Config_Exception(
+              'Directive {' . $dirName . '} not found on block [' . $block . ']'
+          );
+      } else {
+        throw new SLiib_Config_Exception('Block [' . $block . '] not found');
+      }
+    }
+
+  }
+
+
+  /**
+   * Définit une valeur pour une directive du fichier de configuration. Si un
+   * block est précisé, alors la méthode définira la directive contenue dans
+   * le block.
+   * 
+   * @param string           $dirName  Nom de la directive à modifier
+   * @param string           $dirValue Valeur à affecter à la directive
+   * @param string[optional] $block    Nom du block conteneur
+   * 
+   * @throws SLiib_Config_Exception
+   * 
+   * @return void
+   */
+  public function setDirective($dirName, $dirValue, $block=null)
+  {
+    if (is_null($block)) {
+      if (key_exists($dirName, $this->_directives)
+      && !is_array($this->_directives[$dirName])) {
+        $this->_directives[$dirName] = $dirValue;
+      } else {
+        throw new SLiib_Config_Exception(
+            'Directive {' . $dirName . '} does not exist (maybe a block..)'
+        );
+      }
+    } else {
+      if (key_exists($block, $this->_directives)
+      && is_array($this->_directives[$block])) {
+        if (key_exists($dirName, $this->_directives[$block]))
+          $this->_directives[$block][$dirName] = $dirValue;
+        else
+          throw new SLiib_Config_Exception(
+              'Directive {' . $dirName . '} not found on block [' . $block . ']'
+          );
+      } else {
+        throw new SLiib_Config_Exception('Block [' . $block . '] not found');
+      }
+    }
+
+  }
+
+
+  /**
+   * Supprime les espaces indésirables
+   * 
+   * @param string $string Chaine de caractères à nettoyer.
+   * 
+   * @return string La chaine nettoyée
+   */
+  protected function _cleanString($string)
+  {
+    //Delete tabulation
+    $count = 1;
+    while ($count != 0)
+      $string = str_replace("\t", ' ', $string, $count);
+
+    //Delete double space into the string
+    $count = 1;
+    while ($count != 0)
+      $string = str_replace('  ', ' ', $string, $count);
+
+    //Delete space around the string
+    $string = trim($string);
+
+    return $string;
+
+  }
+
+
+}
---
+++ php/library/Config/Ini.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * This source file is part of SLiib.
+ *
+ * SLiib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SLiib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with SLiib. If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ *
+ * PHP version 5
+ *  
+ * @category SLiib
+ * @package  Library
+ * @author   Sliim <sliim@mailoo.org>
+ * @license  GNU/GPL http://www.gnu.org/licenses/gpl-3.0.html
+ * @version  Release: 0.1
+ * @link     http://www.sliim-projects.eu
+ */
+
+/**
+ * SLiib_Config_Ini
+ * 
+ * @package Config
+ */
+class SLiib_Config_Ini extends SLiib_Config
+{
+
+
+  /**
+   * Constructeur. Charge le fichier ini passé en paramètre
+   * 
+   * @param string $file Fichier à charger
+   * 
+   * @return void
+   */
+  public function __construct($file)
+  {
+    parent::__construct($file);
+
+    $this->_readIni();
+
+  }
+
+
+  /**
+   * Récupère l'ensemble de la configuration.
+   * 
+   * @return array Tableau comportant l'ensemble de la configuration.
+   */
+  public function getAll()
+  {
+    return $this->_directives;
+
+  }
+
+
+  /**
+   * Réecrit le fichier de configuration
+   * 
+   * @throws SLiib_Config_Exception
+   * 
+   * @return void
+   */
+  public function rewriteConfig()
+  {
+    $newContent = '';
+    foreach ($this->_directives as $key => $value) {
+      if (is_array($value)) {
+        $newContent .= '[' . $key . ']' . "\r\n";
+        foreach ($value as $dirName => $dirValue) {
+          $newContent .= $dirName . ' = ' . $dirValue . "\r\n";
+        }
+      } else {
+        $newContent .= $key . ' = ' . $value . "\r\n";
+      }
+    }
+
+    $fp = fopen($this->_configFile, 'w');
+    if (!$fp)
+      throw new SLiib_Config_Exception(
+          'Cannot open configuration file ' .
+          $this->_configFile . ' in write mode.'
+      );
+
+    if (!fwrite($fp, $newContent)) {
+      fclose($fp);
+
+      throw new SLiib_Config_Exception(
+          'Cannot write in configuration file ' . $this->_configFile
+      );
+    }
+
+    fclose($fp);
+
+  }
+
+
+  /**
+   * Enregistre les données du fichier .ini dans le tableau $_directives
+   * 
+   * @throws SLiib_Config_Exception
+   * 
+   * @return void
+   */
+  private function _readIni()
+  {
+    $fp = fopen($this->_configFile, 'r');
+    if (!$fp)
+      throw new SLiib_Config_Exception(
+          'Opening file ' . $this->_configFile . ' failed'
+      );
+
+    //Block courant du fichier
+    $block = '';
+
+    while ($line = fgets($fp, 256)) {
+      $line = $this->_cleanString($line);
+      if (!empty($line)) {
+        switch ($line[0]) {
+          case '#':
+              break;
+          case '[':
+            $block = str_replace(array('[', ']'), '', $line);
+            if (key_exists($block, $this->_directives)) {
+              fclose($fp);
+
+              throw new SLiib_Config_Exception(
+                  'Ini error : Block name [' . $block .
+                  '] is already used in the ini file'
+              );
+            }
+
+            $this->_directives[$block] = array();
+              break;
+          default:
+            $data      = explode('=', $line);
+            $directive = $this->_cleanString($data[0]);
+            $value     = $this->_cleanString($data[1]);
+
+            if (empty($block)) {
+              if (key_exists($directive, $this->_directives)) {
+                fclose($fp);
+
+                throw new SLiib_Config_Exception(
+                    'Ini error : Directive name {' .
+                    $directive . '} is already used in the ini file'
+                );
+              }
+
+              $this->_directives[$directive] = $value;
+            } else {
+              if (key_exists($directive, $this->_directives[$block])) {
+                fclose($fp);
+
+                throw new SLiib_Config_Exception(
+                    'Ini error : Directive name is already used' .
+                    ' in the ini file [directive => ' . $directive .
+                    ', block => ' . $block . ']'
+                );
+              }
+
+              $this->_directives[$block][$directive] = $value;
+            }
+              break;
+        }
+      }
+    }
+
+    fclose($fp);
+
+  }
+
+
+}
