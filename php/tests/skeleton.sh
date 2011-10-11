49ee79e76f8b093449de950f2bedb40c8381e00a
Sliim <sliim@mailoo.org>
2011-10-11 21:26:42 +0200

    Script de génération de skeleton pour les tests

--- php/tests/skeleton.sh
+++ php/tests/skeleton.sh
@@ -5,8 +5,29 @@ then
   exit 1
 fi
 
-current_dir=`pwd`
-cd ../library
-phpunit --skeleton $1
+ext=".php"
+pwd=`pwd`
+generate_file=$1"Test"$ext
+deep=`echo $1 | sed 's/_/ /g' | wc -w`
+dir=`echo $1 | cut -f -$(($deep-1)) -d _ | sed 's/_/\//g'`
+file=`echo $1 | cut -f $deep -d _`"Test"$ext
 
-#TODO Move generate skeleton!
+cd ../library/
+phpunit --skeleton-test $1
+
+if test $? -ne 0
+then
+  echo 'PHPUnit returned an error when generating the skeleton'
+  echo 'Exiting..'
+  exit 2
+fi
+
+if test ! -d $pwd/$dir
+then
+  mkdir -p $pwd/$dir
+fi
+mv $dir/$generate_file $pwd/$dir/$file
+
+cd $pwd
+echo "done."
+exit 0
