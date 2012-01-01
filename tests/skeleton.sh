da26bed0038e315e04042a1b3d71167e9d5e20fa
Sliim <sliim@mailoo.org>
2012-01-01 22:01:00 +0100

    closes #79 - Using namespace for unit tests skeleton generator.

--- tests/skeleton.sh
+++ tests/skeleton.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 if test $# -ne 1
 then
-    echo "You must specify a class name!"
+    echo >&2 "You must specify a class name!"
     exit 1
 fi
 
 ext=".php"
 pwd=`pwd`
-generate_file=$1"Test"$ext
-deep=`echo $1 | sed 's/_/ /g' | wc -w`
+deep=`echo $1 | sed "s/\// /g" | wc -w`
 
 if test $deep -lt 2
 then
-    echo "Class name $1 invalid. Exiting.."
+    echo >&2 "Class name $1 invalid. Exiting.."
     exit 2
 fi
 
-dir=`echo $1 | cut -f -$(($deep-1)) -d _ | sed 's/_/\//g'`
-file=`echo $1 | cut -f $deep -d _`"Test"$ext
+dir=`echo $1 | cut -f -$(($deep-1)) -d /`
+class=$(echo $1 | sed -e "s/\//\\\/")
+file=`echo $1 | cut -f $deep -d /`"Test"$ext
 destination_directory=$pwd/library/$dir
 
 if test -f $destination_directory/$file
@@ -27,23 +27,22 @@ then
 fi
 
 cd ../library/
-phpunit --bootstrap $pwd/Bootstrap.php --skeleton-test $1
+phpunit --bootstrap $pwd/Bootstrap.php --skeleton-test $class
 
 if test $? -ne 0
 then
-    echo 'PHPUnit returned an error when generating the skeleton'
+    echo >&2 "PHPUnit returned an error when generating the skeleton"
     cd $pwd
-    echo 'Exiting..'
+    echo "Exiting.."
     exit 4
 fi
-
-sed -i s/\{className\}/$1/i $dir/$generate_file
 
 if test ! -d $destination_directory
 then
     mkdir -p $destination_directory
 fi
-mv $dir/$generate_file $destination_directory/$file
+
+mv $dir/$file $destination_directory/$file
 
 cd $pwd
 echo "done."
